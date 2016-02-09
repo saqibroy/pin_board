572136c76f2c5432ab637968911aa85924c6d701
saqib sohail <saqibsohail007@gmail.com>
2016-02-09 16:10:18 +0500

    with Devise

--- Gemfile
+++ Gemfile
@@ -4,6 +4,7 @@ gem 'coffee-script-source', '1.8.0'
 gem 'haml', '~> 4.0', '>= 4.0.7'
 gem 'simple_form', '~> 3.2', '>= 3.2.1'
 gem 'bootstrap-sass', '~> 3.3', '>= 3.3.6'
+gem 'devise', '~> 3.5', '>= 3.5.5'
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '4.2.5'
 # Use sqlite3 as the database for Active Record
--- app/controllers/pins_controller.rb
+++ app/controllers/pins_controller.rb
@@ -4,10 +4,10 @@ class PinsController < ApplicationController
 		@pins= Pin.all.order("created_at DESC")
 	end
 	def new
-		@pin= Pin.new
+		@pin= current_user.pins.build
 	end
 	def create
-		@pin= Pin.new(pin_params)
+		@pin= current_user.pins.build(pin_params)
 		if @pin.save
 			redirect_to @pin, notice: "Pin successfully created"
 		else
